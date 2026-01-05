a6714a338266909452a1362af820c5b1a8eeb8bd
Tong Ye <ytttkxo03@gmail.com>
2026-01-05 15:54:44 +0100

    feat: add cache for shop queries

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -1,15 +1,22 @@
 package com.hmdp.controller;
 
 
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.service.IShopTypeService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_TTL;
 
 /**
  * <p>
@@ -25,10 +32,33 @@ public class ShopTypeController {
     @Resource
     private IShopTypeService typeService;
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("list")
     public Result queryTypeList() {
-        List<ShopType> typeList = typeService
-                .query().orderByAsc("sort").list();
+        String key = CACHE_SHOP_TYPE_KEY;
+
+        String json = stringRedisTemplate.opsForValue().get(key);
+
+        if (StringUtils.isNotBlank(json)) {
+            List<ShopType> typeList = JSONUtil.toList(JSONUtil.parseArray(json), ShopType.class);
+            return Result.ok(typeList);
+        }
+
+        List<ShopType> typeList = typeService.query().orderByAsc("sort").list();
+
+        if (typeList ==  null || typeList.isEmpty()) {
+            return Result.fail("shop type list is empty");
+        }
+
+        stringRedisTemplate.opsForValue().set(
+                key,
+                JSONUtil.toJsonStr(typeList),
+                CACHE_SHOP_TYPE_TTL,
+                TimeUnit.MINUTES
+        );
+
         return Result.ok(typeList);
     }
 }
--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 /**
  * <p>
@@ -17,4 +25,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+        String key = CACHE_SHOP_KEY + id;
+        // 1. Query shop data from Redis cache
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+
+        // 2. Check whether the data exists
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 3. If it exists, return it directly
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        // 4. If it does not exist, query the database by id
+        Shop shop = getById(id);
+        // 5. If the data does not exist in the database, return an error
+        if (shop == null) {
+            return Result.fail("the shop does not exist!");
+        }
+        // 6. If it exists, write the data into Redis
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop));
+
+        // 7. Return the result
+        return Result.ok(shop);
+    }
 }
