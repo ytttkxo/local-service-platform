4afab80813baeccd5996e1c133aaeff0cc3621da
Tong Ye <ytttkxo03@gmail.com>
2026-01-14 13:20:02 +0100

    feat: implement explorer notes publish, view, like and ranking features

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,10 +1,29 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
 
 /**
  * <p>
@@ -17,4 +36,105 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+    @Resource
+    private IUserService userService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(blog -> {
+            this.queryBlogUser(blog);
+            this.isBlogLiked(blog);
+        });
+        return Result.ok(records);
+    }
+
+    @Override
+    public Result queryBlogById(Long id) {
+        // 1. Query blog
+        Blog blog = getById(id);
+        if(blog == null){
+            return Result.fail("Blog doesn't exist");
+        }
+        // 2. Query about the relevant users
+        queryBlogUser(blog);
+        // 3. Check if the blog is liked
+        isBlogLiked(blog);
+        return Result.ok(blog);
+    }
+
+    private void isBlogLiked(Blog blog) {
+        // 1. Get the logged-in user
+        UserDTO user = UserHolder.getUser();
+        if (user == null) {
+            return;
+        }
+        Long userId = user.getId();
+        // 2. Check whether the current user has already liked
+        String key = "blog:liked:" + blog.getId();
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        blog.setIsLike(score != null);
+    }
+
+    @Override
+    public Result likeBlog(Long id) {
+        // 1. Get the logged-in user
+        Long userId = UserHolder.getUser().getId();
+        // 2. Check whether the current user has already liked
+        String key = BLOG_LIKED_KEY + id;
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        if (score == null) {
+            // 3. If not liked yet, perform like
+            // 3.1 Increment the like count in the database by 1
+            boolean isSuccess = update().setSql("liked = liked + 1").eq("id", id).update();
+            // 3.2 Save the user to the Redis zset
+            if (isSuccess) {
+                stringRedisTemplate.opsForZSet().add(key, userId.toString(), System.currentTimeMillis());
+            }
+        } else {
+            // 4. If already liked, unlike
+            // 4.1 Decrement the like count in the database by 1
+            boolean isSuccess = update().setSql("liked = liked - 1").eq("id", id).update();
+            // 4.2 Remove the user from the Redis set
+            stringRedisTemplate.opsForZSet().remove(key, userId.toString());
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result queryBlogLikes(Long id) {
+        String key = BLOG_LIKED_KEY + id;
+        // 1. Query the top 5 users who liked the post (ZRANGE key 0 4)
+        Set<String> top5 = stringRedisTemplate.opsForZSet().range(key, 0, 4);
+        if (top5 == null || top5.isEmpty()) {
+            return Result.ok(Collections.emptyList());
+        }
+        // 2. Parse the user IDs
+        List<Long> ids = top5.stream().map(Long::valueOf).collect(Collectors.toList());
+        String idStr = StrUtil.join(",", ids);
+        // 3. Query user information based on user IDs
+        List<UserDTO> userDTOS = userService.query()
+                .in("id", ids).last("ORDER BY FIELD(id," + idStr + ")").list()
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        // 4. Return the result
+        return Result.ok(userDTOS);
+    }
+
+    private void queryBlogUser(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
 }
